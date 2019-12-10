64992b6f65ea26584774187e1a682b7eb6f28990
nareshpatilk <naresh.patil@mindtree.com>
2019-12-10 11:51:56 +0530

    gg

--- src/main/java/com/boot/controllers/HomeContoller.java
+++ src/main/java/com/boot/controllers/HomeContoller.java
@@ -1,5 +1,6 @@
 package com.boot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,11 @@ public class HomeContoller {
 
 	@RequestMapping("/")
 	public String greetHello() {
+		return "DAs spring  boot app";
+	}
+	
+	@GetMapping("/getGreet")
+	public String getGreet() {
 		return "DAs spring  boot app";
 	}
 }
