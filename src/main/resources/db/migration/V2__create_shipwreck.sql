559161c338f86b1345bd08586acd40ac6d5a670b
nareshpatilk <naresh.patil@mindtree.com>
2019-12-10 11:45:34 +0530

    gg

---
+++ src/main/resources/db/migration/V2__create_shipwreck.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Shipwreck(
+	id primary key,
+	name VARCHAR(255),
+	description VARCHAR(555),
+	condition VARCHAR(255),
+	depth INT,
+	latitude DOUBLE,
+	longitude DOUBLE,
+	yearDiscovered INT
+	);
